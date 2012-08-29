584dc3cc891674b3d90badde5cf3fb8a5b2dbfb7
xinhuan <xinhuan@54818f23-f904-4a23-9ffb-91781861375d>
2012-08-29 11:54:44 +0000

    Update version to 3.15. Update TOC. Add locales.

---
+++ trunk/Locales/Locale-ptBR.lua
@@ -0,0 +1,26 @@
+﻿-- IPopBar
+-- ptBR Localization file
+-- Note: The english localization file must be loaded before this file.
+
+if ( GetLocale() ~= "ptBR" ) then return end
+local L = {}
+
+-- Name (short and long) and version of the addon
+L["IPopBar"] = true
+L["Integrated PopBar"] = true
+
+
+
+
+
+-- Convert the "true" entries to the same as the key
+for k, v in pairs(L) do
+	if v == true then
+		L[k] = k
+	end
+end
+
+-- Set the english table as the base lookup table
+IPopBar_Localization = setmetatable(L, {__index = IPopBar_Localization})
+
+-- vim: ts=4 noexpandtab
